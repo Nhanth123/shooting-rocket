606112b1ff1a8ead3bf6f02f3e7abacc91817a24
nhan.trinh <hongnhan50@gmail.com>
2024-03-23 22:05:03 +0700

    Feature: Implement Sound effect.

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ var speed = 300
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
 @onready var rocket_container = $RocketContainer
+@onready var rocket_shot_sound = $RocketShotSound
 
 signal took_damage
 
@@ -33,6 +34,7 @@ func shoot():
 	rocket_container.add_child(rocket_instance)
 	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
+	rocket_shot_sound.play()
 
 func take_damage():
 	emit_signal("took_damage")
